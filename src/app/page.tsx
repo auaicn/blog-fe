8b5297207185f35763d2b0cd4498144ce9a8352a
Kyung Ho, Park <crorowslowpp@gmail.com>
2025-03-06 03:45:27 +0900

    feat: home screen to be blog screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to My Blog</h1>
-      <p>이곳은 나의 블로그와 작업 공간입니다.</p>
-    </div>
-  );
+  redirect("/blog");
 }
