7f474080234f19a08d2da44587eee0543a4d89e3
Kyung Ho, Park <crorowslowpp@gmail.com>
2025-03-07 09:41:25 +0900

    fix: feature name

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -2,7 +2,7 @@ export default function Workspace() {
   return (
     <div>
       <div className="w-full flex gap-[4ox] justify-between">
-        <p className="flex-1">Drafts Collection</p>
+        <p className="flex-1">Links Collection</p>
         <p className="flex-1">Drafts Collection</p>
       </div>
     </div>
