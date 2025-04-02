e164de0c18e047f26ff700d456db78adc3b74761
Kyung Ho, Park <crorowslowpp@gmail.com>
2025-04-03 01:56:29 +0900

    feat: disable login button when pending

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,10 @@ import { useSearchParams } from "next/navigation";
 import { authenticate } from "../../lib/actions";
 
 export default function Page() {
-  const [errorMessage, formAction] = useActionState(authenticate, undefined);
+  const [errorMessage, formAction, pending] = useActionState(
+    authenticate,
+    undefined
+  );
 
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/blog";
@@ -52,6 +55,7 @@ export default function Page() {
         <input type="hidden" name="redirectTo" value={callbackUrl} />
         <button
           type="submit"
+          disabled={pending}
           className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
           Sign in
