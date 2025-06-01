b26172454d045b6bd964b3e20c9b494a6ae7ad9e
Kyung Ho, Park <crorowslowpp@gmail.com>
2025-06-01 11:02:35 +0900

    feat: navigation bar - main 사이 표시

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import { twMerge } from "tailwind-merge";
 import { Navigation } from "./navigation";
+import Footer from "./components/Footer";
 
 export default function MainLayout({
   children,
@@ -9,11 +10,11 @@ export default function MainLayout({
   return (
     <div className="h-screen flex flex-col bg-[#121212] overflow-hidden">
       <Navigation />
-      <div className="h-[60px]" /> {/* 네비게이션 바 높이만큼의 여백 */}
+      {/* 네비게이션 바 높이만큼의 여백 */}
+      <div className="h-[60px] shrink-0" />{" "}
+      <div className="h-[4px] bg-gray-200" />
       <main
-        className={twMerge(
-          "flex-grow min-h-0 p-4 overflow-auto scrollbar-hide"
-        )}
+        className={twMerge("flex-grow min-h-0 overflow-auto scrollbar-hide")}
       >
         <div className="min-h-full flex flex-col scrollbar-hide">
           {children}
